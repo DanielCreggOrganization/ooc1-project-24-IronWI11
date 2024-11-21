17d1a8431d227f09ae90b5ec99871e61a5c1a333
IronWI11 <114396471+IronWI11@users.noreply.github.com>
2024-11-21 16:18:42 +0000

    Getter and Setter Methods added

--- src/ie/atu/car_manager/Car.java
+++ src/ie/atu/car_manager/Car.java
@@ -11,5 +11,47 @@ private boolean IsSold;
 
 
 
+//Getter and setter methods
+
+//Getter methods
+
+public int getVinNumber(){
+    return this.VinNumber;
+}
+
+public String getModel(){
+    return this.Model;
+}
+
+public float getPrice(){
+    return this.Price;
+}
+
+public boolean getIsSold(){
+    return this.IsSold;
+}
+
+
+//Setter methods
+
+public void setVinNumber(int VinNumber){
+    this.VinNumber = VinNumber;
+}
+
+public void setModel(String Model){
+    this.Model = Model;
+}
+
+public void setPrice(float Price){
+    this.Price = Price;
+}
+
+public void setIsSold(boolean IsSold){
+    this.IsSold = IsSold;
+}
+
+
+
+
 
 }
